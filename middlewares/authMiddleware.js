dd4894aabeebf3095780151f1b3043419d53d2ea
Pradeep Shakya <86191551+Pradeep-62@users.noreply.github.com>
2024-07-03 02:17:17 +0530

    Update authMiddleware.js

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -6,7 +6,7 @@ export const requireSignIn = async (req, res, next) => {
   try {
     const decode = JWT.verify(
       req.headers.authorization,
-      process.env.JWT_SECRET
+      JWT_SECRET
     );
     req.user = decode;
     next();
